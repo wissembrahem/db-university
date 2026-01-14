ef7e3a2d96769b0c797d121f6fb1ece0b356fd07
Wissembrahem <wbrahem19@gmail.com>
2026-01-14 16:45:50 +0100

    termine

--- University.sql
+++ University.sql
@@ -11,14 +11,35 @@ WHERE cfu > 10
 
 //terza//
 
+SELECT COUNT(*) AS "RISULTATO"
+FROM `students`
+WHERE timestampdiff(YEAR, `date_of_birth`, CURDATE()) > 30
+
 //quarta//
 
-SELECT COUNT(*) "RISULTATO"
+SELECT COUNT(*) as "RISULTATO"
 FROM courses
 WHERE period = "I semestre" AND YEAR = 1
 
 //quinta//
 
-SELECT COUNT(*) "RISULTATO"
+SELECT COUNT(*) as "RISULTATO"
 FROM `exams`
 WHERE `date` = "2020-06-20" AND `hour` >= "14-00-00"
+
+//sesta//
+
+SELECT COUNT(*) as "RISULTATO"
+FROM `degrees`
+WHERE `level` = "magistrale"
+
+//settima//
+
+SELECT COUNT(*) as "RISULTATO"
+FROM `departments`
+
+//ottava//
+
+SELECT COUNT(*) AS "RISULTATO"
+FROM `teachers`
+WHERE `phone` IS NULL
