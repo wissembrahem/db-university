24ddbaf686fbd05a51dcde25d3edae34ed4ef87c
Wissembrahem <wbrahem19@gmail.com>
2026-01-16 10:14:23 +0100

    sesta

--- UniversitySecondDay.sql
+++ UniversitySecondDay.sql
@@ -45,3 +45,17 @@ INNER JOIN `course_teacher`
 ON `courses`.`id` = `course_teacher`.`course_id`
 INNER JOIN  `teachers`
 ON `teachers`.`id` = `course_teacher`.`teacher_id`
+
+//sesta //
+
+SELECT DISTINCT `teachers`.*, `departments`.*
+FROM `teachers`
+INNER JOIN `course_teacher`
+ON `teachers`.`id` = `course_teacher`.`teacher_id`
+INNER JOIN `courses`
+ON `course_teacher`.`course_id` = `courses`.`id`
+INNER JOIN  `degrees`
+ON `courses`.`degree_id` = `degrees`.`id`
+INNER JOIN `departments`
+ON `degrees`.`department_id` = `departments`.`id`
+WHERE `departments`.`name` = "Dipartimento di Matematica"
